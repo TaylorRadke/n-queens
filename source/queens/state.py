51d15f33ba0eba75dd11a95aedb75673cc94315c
Taylor Radke <taylorradke98@gmail.com>
2019-03-07 21:38:10 +1000

    removes row and column moves that are blocked by another queen

--- source/queens/state.py
+++ source/queens/state.py
@@ -22,6 +22,7 @@ class STATE(object):
     
         self.queen_state_space = self.enumerate_queen_state_space()
         self.state_space = self.enumerate_state_space()
+        self.state_row_map, self.state_col_map, self.queen_row_map, self.queen_col_map = self.map_states() 
 
     def __eq__(self,other):
         return self.state in all(other.get_state())
@@ -73,6 +74,26 @@ class STATE(object):
     def enumerate_queen_state_space(self):
         return [Queen(i) for i in self.state]
 
+    def map_states(self):
+        states = self.state_space
+        queens = self.state
+
+        map_state_space_rows = [[] for i in range(self.n)]
+        map_state_space_cols = [[] for i in range(self.n)]
+
+        map_queen_rows = [[] for i in range(self.n)]
+        map_queen_cols = [[] for i in range(self.n)]
+
+        for i in states:
+            map_state_space_rows[i[0]].append(i[1])
+            map_state_space_cols[i[1]].append(i[0])
+
+        for i in queens:
+            map_queen_rows[i[0]].append(i[1])
+            map_queen_cols[i[1]].append(i[0])
+
+        return map_state_space_rows,map_state_space_cols,map_queen_rows,map_queen_cols
+
     def queens_diagonal(self,a,b):
             row_diff = abs(a[0] - b[0])
             col_diff = abs(a[1] - b[1])
@@ -102,6 +123,31 @@ class STATE(object):
                         return True
         return False
 
+    def row_move_blocked(self,row,curr_col,new_col):
+        """Check if left or right move is blocked by another queen"""
+        for i in self.queen_row_map[row]:
+            # Left move blocked
+            if i > new_col and i < curr_col:
+                print("Blocked left")
+                return True
+            # Right move blocked
+            elif i < new_col and i > curr_col:
+                print("Blocked right")
+                return True
+        return False
+
+    def col_move_blocked(self,col,curr_row,new_row):
+        """Check if new up or down move is blocked by another queen"""
+
+        for i in self.queen_col_map[col]:
+            # Down move blocked
+            if i > new_row and i < curr_row:
+                return True
+            # Up move blocked
+            elif i < new_row and i > curr_row:
+                return True
+        return False
+
     def enumerate_legal_successor_states(self):
         """
         Finds all possible legal moves for each queen on the board
@@ -109,17 +155,24 @@ class STATE(object):
 
         queens = self.state
         states = self.state_space
-        
-        for queen in queens[:-1]:
-            possible_moves = []
 
+        for queen in queens[0:1]:
+            print(queen)
+            transitions = []
             for state in states:
-                if state[0] == queen[0] or state[1] == queen[1]:
-                    possible_moves.append(state)
-                if self.queens_diagonal(queen,state):
-                    possible_moves.append(state)
+                #Add transition if on same row
+                if state[0] == queen[0]:
+                    if not self.row_move_blocked(queen[0],queen[1],state[1]):
+                        transitions.append(state)
+                #Add transition if in same column
+                elif state[1] == queen[1]:
+                    if not self.col_move_blocked(queen[1],queen[0],state[0]):
+                        transitions.append(state)
+                #Add transition if diagonal
+                # elif self.queens_diagonal(queen,state):
+                #     transitions.append(state)
+            
 
-            print(possible_moves)
             
 
                 
