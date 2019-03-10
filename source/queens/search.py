b74421592c2de3d47ba2e504703dece94aaddbd0
Taylor Radke <taylorradke98@gmail.com>
2019-03-10 23:31:15 +1000

    moves solution printing to seperate function, also check if state is solution when popped off queue

--- source/queens/search.py
+++ source/queens/search.py
@@ -11,6 +11,7 @@ class Search(object):
         self.frontier = None
         self.solutions = 0
 
+    #Add hash of state to explored
     def add_explored_state(self,action):
         self.explored.add(hash(frozenset(action)))
 
@@ -18,10 +19,20 @@ class Search(object):
     def state_explored(self,state):
         return hash(frozenset(state)) in self.explored
     
+    #Get number of combinations possible for an n * n board
+    # nCr(n) = (n^2)!
+    #       ------------
+    #       n!(n^2 - n)!
     def n_combinations(self):
         n = self.n*self.n
         r = self.n
         return factorial(n) / factorial(r) / factorial(n-r)
+
+    def solution_found(self,solution):
+        self.solutions += 1
+        print('\n\n')
+        State(self.n,solution).print_state()
+        print("-"*80,'\n') 
 
     def get_actions(self,parent,ds_push):
         for action in list(State(self.n,parent).enumerate_actions()):
@@ -31,11 +42,7 @@ class Search(object):
                 self.add_explored_state(action)
                 #Check if state is a goal state
                 if not State(self.n,action).state_in_conflict():
-                    self.solutions += 1
-                    #Print Solution
-                    print()
-                    State(self.n,action).print_state()
-                    print("-"*80,'\n') 
+                    self.solution_found(action)
                 #Add state to frontier
                 else:
                     ds_push(action)
@@ -46,6 +53,8 @@ class BFS(Search):
     def __init__(self,n,initial_state=None):
         super().__init__(n,initial_state)
         self.frontier = Queue()
+
+        #Push inital state to the queue
         self.frontier.put(self.initial_state.get_state())
 
     def search(self):
@@ -56,9 +65,12 @@ class BFS(Search):
         while not self.frontier.empty():
             print("n = {},   Solutions found: {},   States Checked: {}/{},  States Queued: {}"
             .format(self.n,self.solutions,len(self.explored),combinations,self.frontier._qsize()),end='\r')
+                
             state = self.frontier.get()
+            if not State(self.n,state).state_in_conflict():
+                self.solution_found(state)
             self.add_explored_state(state)
             self.get_actions(state,self.frontier.put)
-
+        print()
         search_time = time() - start
         print("Search Time: ", search_time, "\tStates explored: ",len(self.explored),"\tSolutions Found: ",self.solutions)
