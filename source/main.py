186880d19ff956d79365dd624f5a3c7d323bdda0
Taylor Radke <taylorradke98@gmail.com>
2019-03-10 23:05:04 +1000

    added combinations to show number of possible state combinations

--- source/main.py
+++ source/main.py
@@ -1,22 +1,9 @@
 import queens.search as search
 import sys
-import csv
-import os
 
 def main():
-    # n = int(sys.argv[1])
-    # sample_size = int(sys.argv[2])
-    # csv_ = os.path.join("results",'results.csv')
-    search.BFS(5).search()
-    
-    # n = 5
-    # sample_size = 1
-    # with open("results\\results.csv",'w',newline='') as csv_file:
-    #     csv_writer = csv.writer(csv_file,)
-    #     for n in range(n,n+1):
-    #         for _ in range(sample_size):
-    #             search.BFS(n,logger=csv_writer).search()
-    #             csv_file.flush()
+    n = int(sys.argv[1])
+    search.BFS(n).search()
 
 if __name__ == "__main__":
-    main()
+    main() 
--- source/queens/search.py
+++ source/queens/search.py
@@ -1,11 +1,11 @@
 from queue import Queue
 from queens.state import State
 from time import time
+from math import factorial
 
 class Search(object):
-    def __init__(self,n,initial_state=None,logger=None):
+    def __init__(self,n,initial_state=None):
         self.n = n
-        self.logger = logger
         self.initial_state = State(self.n,initial_state)
         self.explored = set({})
         self.frontier = None
@@ -18,42 +18,47 @@ class Search(object):
     def state_explored(self,state):
         return hash(frozenset(state)) in self.explored
     
+    def n_combinations(self):
+        n = self.n*self.n
+        r = self.n
+        return factorial(n) / factorial(r) / factorial(n-r)
+
     def get_actions(self,parent,ds_push):
         for action in list(State(self.n,parent).enumerate_actions()):
-            #Check if action is a goal state
+            #Check if state has already been explored
             if not self.state_explored(action):
+                #Add state to explored
+                self.add_explored_state(action)
+                #Check if state is a goal state
                 if not State(self.n,action).state_in_conflict():
                     self.solutions += 1
-                    self.add_explored_state(action)
+                    #Print Solution
                     print()
                     State(self.n,action).print_state()
-                    print("-"*50,'\n') 
+                    print("-"*80,'\n') 
+                #Add state to frontier
                 else:
-                    if not self.state_explored(action):
-                        ds_push(action)
+                    ds_push(action)
 
 class BFS(Search):
     """Breadth first search is a tree search which implements a queue to find a solution"""
 
-    def __init__(self,n,initial_state=None,logger=None):
-        super().__init__(n,initial_state,logger)
+    def __init__(self,n,initial_state=None):
+        super().__init__(n,initial_state)
         self.frontier = Queue()
         self.frontier.put(self.initial_state.get_state())
 
     def search(self):
         """Performs the breadth-first to find a solution to the initial state"""
         start = time()
+        combinations = int(self.n_combinations())
 
         while not self.frontier.empty():
-            print("n = {},   Solutions found: {},    States Checked: {}, Frontier: {}".format(self.n,self.solutions,len(self.explored),self.frontier._qsize()),end='\r')
+            print("n = {},   Solutions found: {},   States Checked: {}/{},  States Queued: {}"
+            .format(self.n,self.solutions,len(self.explored),combinations,self.frontier._qsize()),end='\r')
             state = self.frontier.get()
             self.add_explored_state(state)
             self.get_actions(state,self.frontier.put)
 
-
         search_time = time() - start
         print("Search Time: ", search_time, "\tStates explored: ",len(self.explored),"\tSolutions Found: ",self.solutions)
-        if self.logger:
-            self.logger.writerow([self.n,search_time,len(self.explored),\
-                self.solutions,self.initial_state.get_state()])
-        del self
