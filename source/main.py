7c961f13a76cdd395be5d59d6a73eedd5d1d0353
Taylor Radke <taylorradke98@gmail.com>
2019-03-10 12:29:17 +1000

    removed some console printing and unused class functions

--- source/main.py
+++ source/main.py
@@ -2,6 +2,7 @@ import queens.search as search
 import sys
 import csv
 import os
+
 def main():
     n = int(sys.argv[1])
     sample_size = int(sys.argv[2])
@@ -11,10 +12,10 @@ def main():
         csv_writer = csv.writer(csv_file,)
         for n in range(1,n+1):
             for _ in range(sample_size):
-                tree = search.BFS(n,logging=csv_writer)
-                tree.search()
-                del tree
-                csv_file.flush()
+            
+                search.BFS(n,logger=csv_writer).search()
 
+                csv_file.flush()
+                
 if __name__ == "__main__":
     main()
--- source/queens/search.py
+++ source/queens/search.py
@@ -2,20 +2,19 @@ from queue import Queue
 from queens.state import State
 from time import time
 import datetime
+import sys
 
 class Search(object):
-    def __init__(self,n,initial_state=None,logging=None):
+    a = None
+    def __init__(self,n,initial_state=None,logger=None):
         self.n = n
-        self.logging = logging
+        self.logger = logger
         self.initial_state = State(self.n,initial_state)
 
         self.goal_reached = False
         self.explored = set({})
         self.frontier = None
         self.solution = None
-
-    def print_solution(self):
-        print(self.solution.print_state())
 
     def add_explored_state(self,state):
         self.explored.add(hash(frozenset(state)))
@@ -26,15 +25,13 @@ class Search(object):
             return False
         return True
 
-    def found_solution(self,solution_state):
-        self.solution = solution_state
-        self.goal_reached = True
-
     def map_new_state(self,parent,ds_push):
         for action in list(State(self.n,parent).enumerate_actions()):
+            #Check if action is a goal state
             if not State(self.n,action).state_in_conflict():
-                self.found_solution(action)
-            #Check if state not explored
+                self.goal_reached = True
+                self.solution = action
+
             elif not self.state_explored(action):
             #Add new state to data structure (queue,stack...etc)
                 ds_push(action)
@@ -45,34 +42,33 @@ class Search(object):
 class BFS(Search):
     """Breadth first search is a tree search which implements a queue to find a solution"""
 
-    def __init__(self,n,initial_state=None,logging=None):
-        super().__init__(n,initial_state,logging)
+    def __init__(self,n,initial_state=None,logger=None):
+        super().__init__(n,initial_state,logger)
         self.frontier = Queue()
         self.frontier.put(self.initial_state.get_state())
-
+    
+    
     def search(self):
         """Performs the breadth-first to find a solution to the initial state"""
         print("n = ", self.n)
+        print("Initial State:   ")
         self.initial_state.print_state()
         start = time()
         print("Search Started:  ",datetime.datetime.now())
         while not self.goal_reached and not self.frontier.empty():
             state = self.frontier.get()
             self.add_explored_state(state)
-
             self.map_new_state(state,self.frontier.put)
-            print("States Explored: {}".format(len(self.explored)),end='\r',flush=True)
 
         search_time = time()-start
-        print()
-        print("Time taken: {} seconds".format(search_time))
-        print("Search Finished: ", datetime.datetime.now())
         if self.frontier.empty():
             print("No solution")
         else:
             print("Solution found:")
             State(self.n,self.solution).print_state()
-
+        print()
+        print("Search Finished: ", datetime.datetime.now())
+        print("Time taken: {} seconds".format(search_time))
         print()
         print("="*50)
         print()
@@ -80,6 +76,7 @@ class BFS(Search):
         if not self.solution:
             self.solution = "None"
 
-        if self.logging:
-            self.logging.writerow([self.n,search_time,len(self.explored),\
+        if self.logger:
+            self.logger.writerow([self.n,search_time,len(self.explored),\
                 self.initial_state.get_state(),self.solution])
+        del self
